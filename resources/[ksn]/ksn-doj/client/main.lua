e973cd0130f47a0ffe625c81971f29a01402df57
Ksnarf <44852455+Ksnarf@users.noreply.github.com>
2022-10-18 19:36:18 -0700

    Create main.lua
    for ksn-doj

---
+++ resources/[ksn]/ksn-doj/client/main.lua
@@ -0,0 +1,97 @@
+QBCore = exports['qb-core']:GetCoreObject()
+isHandcuffed = false
+cuffType = 1
+isEscorted = false
+PlayerJob = {}
+onDuty = false
+local DutyBlips = {}
+
+local function CreateDutyBlips(playerId, playerLabel, playerJob, playerLocation)  --playerblips for those in DOJ
+    local ped = GetPlayerPed(playerId)
+    local blip = GetBlipFromEntity(ped)
+    if not DoesBlipExist(blip) then
+        if NetworkIsPlayerActive(playerId) then
+            blip = AddBlipForEntity(ped)
+        else
+            blip = AddBlipForCoord(playerLocation.x, playerLocation.y, playerLocation.z)
+        end
+        SetBlipSprite(blip, 1)
+        ShowHeadingIndicatorOnBlip(blip, true)
+        SetBlipRotation(blip, math.ceil(playerLocation.w))
+        SetBlipScale(blip, 1.0)
+        if playerJob == "doj" then
+            SetBlipColour(blip, 38)
+        else
+            SetBlipColour(blip, 5)
+        end
+        SetBlipAsShortRange(blip, true)
+        BeginTextCommandSetBlipName('STRING')
+        AddTextComponentString(playerLabel)
+        EndTextCommandSetBlipName(blip)
+        DutyBlips[#DutyBlips+1] = blip
+    end
+
+    if GetBlipFromEntity(PlayerPedId()) == blip then
+        -- Ensure we remove our own blip.
+        RemoveBlip(blip)
+    end
+end
+
+AddEventHandler('QBCore:Client:OnPlayerLoaded', function() --
+    local player = QBCore.Functions.GetPlayerData()
+    PlayerJob = player.job
+    onDuty = player.job.onduty
+    isHandcuffed = false
+    TriggerServerEvent("QBCore:Server:SetMetaData", "ishandcuffed", false)
+    TriggerServerEvent("police:server:SetHandcuffStatus", false)
+    TriggerServerEvent("police:server:UpdateBlips")
+    TriggerServerEvent("police:server:UpdateCurrentCops")
+
+    if player.metadata.tracker then
+        local trackerClothingData = {
+            outfitData = {
+                ["accessory"] = {
+                    item = 13,
+                    texture = 0
+                }
+            }
+        }
+        TriggerEvent('qb-clothing:client:loadOutfit', trackerClothingData)
+    else
+        local trackerClothingData = {
+            outfitData = {
+                ["accessory"] = {
+                    item = -1,
+                    texture = 0
+                }
+            }
+        }
+        TriggerEvent('qb-clothing:client:loadOutfit', trackerClothingData)
+    end
+
+    if PlayerJob and PlayerJob.name ~= "police" then
+        if DutyBlips then
+            for _, v in pairs(DutyBlips) do
+                RemoveBlip(v)
+            end
+        end
+        DutyBlips = {}
+    end
+end)
+
+RegisterNetEvent('QBCore:Client:OnPlayerUnload', function()
+    TriggerServerEvent('police:server:UpdateBlips')
+    TriggerServerEvent("police:server:SetHandcuffStatus", false)
+    TriggerServerEvent("police:server:UpdateCurrentCops")
+    isHandcuffed = false
+    isEscorted = false
+    onDuty = false
+    ClearPedTasks(PlayerPedId())
+    DetachEntity(PlayerPedId(), true, false)
+    if DutyBlips then
+        for _, v in pairs(DutyBlips) do
+            RemoveBlip(v)
+        end
+        DutyBlips = {}
+    end
+end)
