038ea5698aea4989f7a8719b559284ee60b624b4
Ksnarf <44852455+Ksnarf@users.noreply.github.com>
2023-01-07 23:28:21 -0800

    new module [ ksn-crypto ]
    for illegal item purchase, tied to gas prices (maybe part of an IC stock exchange later on)

---
+++ resources/[DO_NOT_RUN]/ksn-crypto/client/main.lua
@@ -0,0 +1,3 @@
+local QBCore = exports['qb-core']:GetCoreObject()
+local requiredItemsShowed = false
+local requiredItems = {[1] = {name = QBCore.Shared.Items["cryptostick"]["name"], image = QBCore.Shared.Items["cryptostick"]["image"]}}
